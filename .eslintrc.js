82aae06858b305eea167777fbccc5a1602b58521
WooYoung Cho <wy1.cho@samsung.com>
2022-01-26 09:47:23 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime"
     ],
     "parserOptions": {
         "ecmaFeatures": {
